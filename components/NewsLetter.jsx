bcda72d873b06ac33f93f35bbc32114c7edabeb3
Himel <shahriarhasan221@gmail.com>
2025-11-26 11:36:30 +0600

    edited newsletter

--- components/NewsLetter.jsx
+++ components/NewsLetter.jsx
@@ -7,9 +7,10 @@ const NewsLetter = () => {
         Subscribe now & get 20% off
       </h1>
       <p className="md:text-base text-gray-500/80 pb-8">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry.
+        Join our newsletter to get exclusive updates, latest product launches,
+        and special discounts straight to your inbox.
       </p>
+
       <div className="flex items-center justify-between max-w-2xl w-full md:h-14 h-12">
         <input
           className="border border-gray-500/30 rounded-md h-full border-r-0 outline-none w-full rounded-r-none px-3 text-gray-500"
